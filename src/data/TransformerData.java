4970eed757ae8aa546e8a7e50fd6b4d3e6f7f1e2
D-Tomics <akm001gaming@gmail.com>
2020-04-06 21:05:55 +0530

    added ability to store on or off state of tr to TransformerData bean

--- src/data/TransformerData.java
+++ src/data/TransformerData.java
@@ -2,44 +2,34 @@ package data;
 
 public class TransformerData {
 
-    private int id;
-    private float current;
-    private float voltage;
-    private float load;
-    private String status;
-    private String loc;
+    private int     id;
+    private float   current;
+    private float   voltage;
+    private float   frequency;
+    private String  status;
+    private String  loc;
+    private boolean isOn;
 
-    public TransformerData(int id,float current, float voltage, String loc, String status) {
-        this.id = id;
-        this.current = current;
-        this.voltage = voltage;
-        this.loc = loc;
-        this.status = status;
-        this.load = voltage/current;
+    public TransformerData(int id,float current, float voltage, float frequency, String loc, String status, boolean isOn) {
+        this.id         = id;
+        this.current    = current;
+        this.voltage    = voltage;
+        this.frequency  = frequency;
+        this.loc        = loc;
+        this.status     = status;
+        this.isOn       = isOn;
     }
 
-    public int getId() {
-        return id;
-    }
+    public int      getId       () { return id;         }
+    public float    getCurrent  () { return current;    }
+    public float    getVoltage  () { return voltage;    }
+    public float    getFrequency() { return frequency;  }
+    public String   getLoc      () { return loc;        }
+    public String   getStatus   () { return status;     }
+    public boolean  isOn        () { return isOn;       }
 
-    public float getCurrent() {
-        return current;
-    }
-
-    public float getVoltage() {
-        return voltage;
-    }
-
-    public String getLoc() {
-        return loc;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public float getLoad() {
-        return load;
+    public String toString() {
+        return "{ id :"+id+", current : "+current+", voltage : "+voltage+", freq : "+frequency+", location : "+loc+", status : "+status+" }";
     }
     
 }
